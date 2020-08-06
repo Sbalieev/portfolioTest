dc60e668c16cb022e0ce6d435fe174a9553d9389
Samir Alieev <36667252+Sbalieev@users.noreply.github.com>
2020-08-06 14:16:55 +0200

    new

--- js/main.js
+++ js/main.js
@@ -1,11 +1,29 @@
+$(function(){  // $(document).ready shorthand
+  $('.monster').fadeIn('slow');
+});
 
-  var typed = new Typed(".animate", {
-    strings: [
-    "Designer",
-    "Designer",
-    "Programmer"
-    ],
-    typeSpeed: 50,
-    backSpeed: 50,
-    loop: true
-  });
+$(document).ready(function() {
+    
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+    
+        /* Check the location of each desired element */
+        $('.hideme').each( function(i){
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},1500);
+                    
+            }
+            
+        }); 
+    
+    });
+    
+});
+
+
